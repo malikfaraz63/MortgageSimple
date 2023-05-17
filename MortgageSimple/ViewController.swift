83254222db33892365c78d7c28d7f82780770f05
malikfaraz63 <malikfaraz63@gmail.com>
2023-05-17 21:54:03 +0100

    Initial Commit

---
+++ MortgageSimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortgageSimple
+//
+//  Created by Faraz Malik on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
